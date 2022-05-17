25d2096abdcc9717eeafcb1ae21e17f828bd1430
michaelfinnerty <michaelfinnerty@localhost>
2022-05-16 20:57:19 -0600

    bits and bytes
    to demonstrate my capacity and interest to a friend, you know, e.g. "to have fun"?

--- clojureDemo.clj
+++ clojureDemo.clj
@@ -1,5 +1,5 @@
 (import com.neuronrobotics.bowlerstudio.BowlerKernel)
 
 (BowlerKernel/speak 
-	"You can write scripts in Clojure"
+	"I am unamused by this forced display of sociality for a fucking learning exercise.  I mean for shit's sake, you're telling me that my fumbling awakward grasping first attempts at experimenting with a new thing need to be shared with the entire fucking world?  Hard pass.  Where's the magic in a world where that's is all?"
 )
